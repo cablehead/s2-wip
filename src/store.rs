7b896dbb3ae659b82dbf9818e34a8c5f0408a96a
Andy Gayton <andy@thecablelounge.com>
2023-07-26 22:15:59 -0400

    wip

--- src/store.rs
+++ src/store.rs
@@ -70,23 +70,35 @@ pub struct DeletePacket {
 }
 
 pub struct Store {
-    db: sled::Db,
+    packets: sled::Tree,
+    content: sled::Tree,
     pub cache_path: String,
 }
 
 impl Store {
     pub fn new(path: &str) -> Store {
         let db = sled::open(std::path::Path::new(path).join("index")).unwrap();
+        let packets = db.open_tree("packets").unwrap();
+        let content = db.open_tree("content").unwrap();
         let cache_path = std::path::Path::new(path)
             .join("cas")
             .into_os_string()
             .into_string()
             .unwrap();
-        Store { db, cache_path }
+        Store { packets, content, cache_path }
     }
 
-    pub fn cas_write(&self, content: &[u8]) -> ssri::Integrity {
-        cacache::write_hash_sync(&self.cache_path, content).unwrap()
+    pub fn cas_write(&self, content: &[u8], mime_type: MimeType) -> ssri::Integrity {
+        let hash = cacache::write_hash_sync(&self.cache_path, content).unwrap();
+        let content = Content {
+            hash: Some(hash.clone()),
+            mime_type,
+            terse: String::from_utf8_lossy(content).into_owned(),
+            tiktokens: content.len(),
+        };
+        let encoded: Vec<u8> = bincode::serialize(&content).unwrap();
+        self.content.insert(hash.to_string(), encoded).unwrap();
+        hash
     }
 
     pub fn cas_read(&self, hash: &ssri::Integrity) -> Option<Vec<u8>> {
@@ -95,11 +107,11 @@ impl Store {
 
     pub fn insert_packet(&mut self, packet: &Packet) {
         let encoded: Vec<u8> = bincode::serialize(&packet).unwrap();
-        self.db.insert(packet.id().to_bytes(), encoded).unwrap();
+        self.packets.insert(packet.id().to_bytes(), encoded).unwrap();
     }
 
     pub fn scan(&self) -> impl Iterator<Item = Packet> {
-        self.db.iter().filter_map(|item| {
+        self.packets.iter().filter_map(|item| {
             item.ok()
                 .and_then(|(_, value)| bincode::deserialize::<Packet>(&value).ok())
         })
